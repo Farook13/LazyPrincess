c1cae1099e5982151a6f8df4f9f20ebc1f5eaa3b
Farook13 <umarulfarook787@gmail.com>
2025-02-23 17:49:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,7 +42,7 @@ request_channel = environ.get('REQUEST_CHANNEL', 'https://t.me/subit23')
 
 # MongoDB information *
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://saidalimuhamed88:iladias2025@cluster0.qt4dv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # LOG CHANNELS *
