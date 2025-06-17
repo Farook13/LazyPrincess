65b03cc495c34d835523cf6dfe5c824fa4c540d4
Farook13 <umarulfarook787@gmail.com>
2025-06-17 10:42:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,8 +41,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 request_channel = environ.get('REQUEST_CHANNEL', 'https://t.me/subit23')
 
 # MongoDB information *
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://batman13:batman13@batman.sawvl.mongodb.net/?retryWrites=true&w=majority&appName=batman")
-DATABASE_NAME = environ.get('DATABASE_NAME', "batman")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://farook:farook@cluster0.aaed9bf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # LOG CHANNELS *
